6f10b9b3b6378c505689aefd3df4916dc75d6f96
Akankshya <edworked2005@gmail.com>
2025-09-22 14:18:09 +0530

    A20 line enabled

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -67,9 +67,17 @@ load32:
    mov fs,ax
    mov gs,ax
    mov ss,ax
+   mov ebp,0x00200000
+   mov esp,ebp
+  
+   ;enabled A20LINE
+   in al,0x92
+   or al,2
+   out 0x92, al
+
    jmp $
 
     
 
-times 510-($ - $$) db 0
-dw 0xAA55
+; times 510-($ - $$) db 0
+; dw 0xAA55
